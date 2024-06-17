23eb0533b5c2ddf853c276ca2691ec41d19ed5d1
benjypng <47936793+benjypng@users.noreply.github.com>
2024-06-17 16:38:19 +0800

    chore: eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import prettierConfig from 'eslint-config-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+)
